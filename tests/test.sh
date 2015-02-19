7dc2b26929d42009bff48592050069bf21862e4b
Miguel <miguel@miguel.localhost>
2015-02-19 00:01:47 +0000

    Improved test bash script

--- tests/test.sh
+++ tests/test.sh
@@ -7,15 +7,23 @@ mono=mono
 for j in *.uk
     do
         n=$(basename $j .uk)
-        rm -rf $n.ukil $n.exe
-        $compiler $j
-        $ilasm $n.ukil 1>/dev/null
-        $mono $n.exe > $n.tmp
+        rm -rf $n.ukil $n.exe $n.tmp $n.err.tmp
+        touch $n.tmp $err.tmp
+        $compiler $j 2> $n.err.tmp
+        if [ $? -eq 0 ]
+            then
+                $ilasm $n.ukil 1>/dev/null
+                $mono $n.exe > $n.tmp
+        fi
         diff -b -B --brief $n.out $n.tmp
         if [ $? -eq 0 ]
             then
-                echo "$n: PASSED";
-        else
-            echo "$n: FAILED"
+                diff -b -B --brief $n.err $n.err.tmp
+                if [ $? -eq 0 ]
+                    then
+                        echo "$n: PASSED";
+                    else
+                        echo "$n: FAILED"
+                fi
         fi
     done
